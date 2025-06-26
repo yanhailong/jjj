b9a159788fa1429fab25514317ce95b6bb122ff7
yaokai <1296172701@qq.com>
2025-06-26 18:34:42 +0800

    fix：测试代码删除

--- client/Assets/LuaScripts/PlatformHall/UILogin/Ctrl/UILoginCtrl.lua
+++ client/Assets/LuaScripts/PlatformHall/UILogin/Ctrl/UILoginCtrl.lua
@@ -21,13 +21,10 @@ end
 function UILoginCtrl:CtrlInit(args)
 	self.super.CtrlInit(self,args);
 	self:InitData()
-	--self.model:Login()
+	self.model:Login()
 	
 	---@type ObjectPoolUtil
 	self.pool=ObjectPoolUtil.New(self.ctrlName)
-
-	require("SingleGames/DragonTigerFight/MVCHead")
-	CtrlManager.SingleShow(CtrlNames.DragonTigerFight)
 end
 
 function UILoginCtrl:InitData()
