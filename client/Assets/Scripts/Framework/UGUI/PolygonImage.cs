fbe7039c5779718a8b3a565749a987b1deb828af
yaokai <1296172701@qq.com>
2025-07-02 14:45:56 +0800

    fret: 不规则图片点击

---
+++ client/Assets/Scripts/Framework/UGUI/PolygonImage.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+/// <summary>
+/// 响应不规则区域点击事件
+/// </summary>
+[RequireComponent(typeof(PolygonCollider2D))]
+public class PolygonImage : Image {
+    PolygonCollider2D polygonCollider2D;
+    protected override void Awake()
+    {
+        base.Awake();
+        polygonCollider2D = this.GetComponent<PolygonCollider2D>();
+    }
+
+    public override bool IsRaycastLocationValid(Vector2 screenPoint, Camera eventCamera)
+    {
+        
+        if (eventCamera != null) screenPoint = eventCamera.ScreenToWorldPoint(screenPoint);
+        return polygonCollider2D.OverlapPoint(screenPoint);
+    }
+
+}
