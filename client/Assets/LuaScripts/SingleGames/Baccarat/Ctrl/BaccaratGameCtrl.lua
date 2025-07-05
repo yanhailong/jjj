ce7273903e50603a523f8a8c30202b87550ce087
wangjian <1652327278@qq.com>
2025-07-05 14:08:19 +0800

    feat：红黑大战游戏修改

--- client/Assets/LuaScripts/SingleGames/Baccarat/Ctrl/BaccaratGameCtrl.lua
+++ client/Assets/LuaScripts/SingleGames/Baccarat/Ctrl/BaccaratGameCtrl.lua
@@ -982,7 +982,10 @@ end
 function BaccaratGameCtrl:RealCloseDestroy()
 	self.super.RealCloseDestroy(self);
 	TimerManager.StopAllTimer(self)
-	
+	if self.SettlementCor then
+		coroutine.stop(self.SettlementCor)
+		self.SettlementCor=nil
+	end
 	if self.flickerSequence~=nil then
 		self.flickerSequence:Kill();
 	end
--- client/Assets/LuaScripts/SingleGames/RoyalWar/Ctrl/RoyalWarGameCtrl.lua
+++ client/Assets/LuaScripts/SingleGames/RoyalWar/Ctrl/RoyalWarGameCtrl.lua
@@ -927,7 +927,10 @@ end
 function RoyalWarGameCtrl:RealCloseDestroy()
 	self.super.RealCloseDestroy(self);
 	TimerManager.StopAllTimer(self)
-
+	if self.SettlementCor then
+		coroutine.stop(self.SettlementCor)
+		self.SettlementCor=nil
+	end
 	if self.flickerSequence~=nil then
 		self.flickerSequence:Kill();
 	end
