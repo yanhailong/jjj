6ef4b1e125090cd56a82ae03b2a4a184dbe6c3d5
linfan <123456789@cn.com>
2025-07-26 17:06:35 +0800

    test：删除测试代码

--- client/Assets/LuaScripts/SingleGames/GameTemp1/MVCHead.lua
+++
@@ -1,6 +0,0 @@
-CtrlNames.GameTemp1="GameTemp1";
-CtrlManager.CtrlsCollection[CtrlNames.GameTemp1]={};
-CtrlManager.CtrlsCollection[CtrlNames.GameTemp1].ctrl =require "SingleGames/GameTemp1/Ctrl/GameTemp1Ctrl";
-CtrlManager.CtrlsCollection[CtrlNames.GameTemp1].view=require "SingleGames/GameTemp1/View/GameTemp1View";
-CtrlManager.CtrlsCollection[CtrlNames.GameTemp1].model=require "SingleGames/GameTemp1/Model/GameTemp1Model";
-
